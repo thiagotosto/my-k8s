6043bf2920f8215435c90ccc18a574f71e912a93
Thiago Tosto <thiagotosto@gmail.com>
2026-05-20 12:01:38 -0300

    feat(infra): add cluster_type variable to switch between Kind and GKE

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "thiago-terraform-state"
+    bucket = "juslake-terraform-state"
     prefix = "terraform/state"
   }
   required_providers {
@@ -19,28 +19,44 @@ terraform {
   }
 }
 
-
 provider "google" {
   project = "my-k8s-495416"
 }
 
 provider "null" {}
 
+data "google_client_config" "default" {}
+
+locals {
+  gke_endpoint = try(google_container_cluster.my_cluster[0].endpoint, "")
+  gke_ca_cert = var.cluster_type == "gke" ? try(
+    base64decode(google_container_cluster.my_cluster[0].master_auth[0].cluster_ca_certificate),
+    null
+  ) : null
+}
+
 provider "kubernetes" {
-  config_path    = var.kubeconfig_path
-  config_context = var.kube_context
+  host                   = var.cluster_type == "gke" ? "https://${local.gke_endpoint}" : null
+  token                  = var.cluster_type == "gke" ? data.google_client_config.default.access_token : null
+  cluster_ca_certificate = local.gke_ca_cert
+  config_path            = var.cluster_type == "kind" ? var.kubeconfig_path : null
+  config_context         = var.cluster_type == "kind" ? var.kube_context : null
 }
 
 provider "helm" {
   kubernetes = {
-    config_path    = var.kubeconfig_path
-    config_context = var.kube_context
+    host                   = var.cluster_type == "gke" ? "https://${local.gke_endpoint}" : null
+    token                  = var.cluster_type == "gke" ? data.google_client_config.default.access_token : null
+    cluster_ca_certificate = local.gke_ca_cert
+    config_path            = var.cluster_type == "kind" ? var.kubeconfig_path : null
+    config_context         = var.cluster_type == "kind" ? var.kube_context : null
   }
 }
 
 ## CLUSTER
 resource "kind_cluster" "my-cluster" {
-  name = "my-cluster"
+  count = var.cluster_type == "kind" ? 1 : 0
+  name  = "my-cluster"
   config = <<-EOF
         apiVersion: kind.x-k8s.io/v1alpha4
         kind: Cluster
@@ -49,6 +65,70 @@ resource "kind_cluster" "my-cluster" {
         - role: worker
         - role: worker
     EOF
+}
+
+resource "google_container_cluster" "my_cluster" {
+  count    = var.cluster_type == "gke" ? 1 : 0
+  name     = "my-cluster"
+  location = var.gcp_zone
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+  deletion_protection      = false
+
+  workload_identity_config {
+    workload_pool = "my-k8s-495416.svc.id.goog"
+  }
+}
+
+resource "google_container_node_pool" "system" {
+  count    = var.cluster_type == "gke" ? 1 : 0
+  name     = "system"
+  location = var.gcp_zone
+  cluster  = google_container_cluster.my_cluster[0].name
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
+  }
+
+  node_config {
+    machine_type = "e2-standard-4"
+
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+  }
+}
+
+resource "google_container_node_pool" "spark" {
+  count    = var.cluster_type == "gke" ? 1 : 0
+  name     = "spark"
+  location = var.gcp_zone
+  cluster  = google_container_cluster.my_cluster[0].name
+
+  autoscaling {
+    min_node_count = 0
+    max_node_count = 10
+  }
+
+  node_config {
+    machine_type = "e2-standard-4"
+
+    labels = {
+      pool = "spark"
+    }
+
+    taint {
+      key    = "pool"
+      value  = "spark"
+      effect = "NO_SCHEDULE"
+    }
+
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+  }
 }
 
 ## MODULES
@@ -71,7 +151,6 @@ module "juslake_gcs_bucket" {
 
   bucket_name = "juslake"
 }
-
 
 module "spark-operator" {
   count  = var.spark_operator ? 1 : 0
--- variables.tf
+++ variables.tf
@@ -1,3 +1,25 @@
+variable "cluster_type" {
+  description = "Type of Kubernetes cluster to deploy"
+  type        = string
+  default     = "kind"
+  validation {
+    condition     = contains(["gke", "kind"], var.cluster_type)
+    error_message = "cluster_type must be 'kind' or 'gke'."
+  }
+}
+
+variable "gcp_region" {
+  description = "GCP region for GKE cluster and Artifact Registry"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "gcp_zone" {
+  description = "GCP zone for GKE cluster"
+  type        = string
+  default     = "us-central1-a"
+}
+
 variable "spark_operator" {
   type    = bool
   default = true
