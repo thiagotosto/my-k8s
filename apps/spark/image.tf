0ae730271b66519cc53e5d8db52bf0e176f0212f
Thiago Tosto <thiagotosto@gmail.com>
2026-05-20 13:10:27 -0300

    feat(spark): replace kind image load with Artifact Registry push

--- apps/spark/image.tf
+++ apps/spark/image.tf
@@ -5,8 +5,8 @@ resource "null_resource" "spark_custom_image" {
 
   provisioner "local-exec" {
     command = <<-EOT
-      docker build --pull=false -t spark-lance-gcs:4.0.2 ${path.module}
-      kind load docker-image spark-lance-gcs:4.0.2 --name my-cluster
+      docker build --pull=false -t ${var.ar_repository}/spark-lance-gcs:4.0.2 ${path.module}
+      docker push ${var.ar_repository}/spark-lance-gcs:4.0.2
     EOT
   }
 }
