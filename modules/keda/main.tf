0a82b4f2f79c5b77893585eb1b26a6bb187a7e30
Thiago Tosto <thiagotosto@gmail.com>
2026-06-01 16:00:46 -0300

    feat(keda): add KEDA Helm module

---
+++ modules/keda/main.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "keda" {
+  name             = "keda"
+  repository       = "https://kedacore.github.io/charts"
+  chart            = "keda"
+  version          = var.chart_version
+  namespace        = "keda"
+  create_namespace = true
+}
---
+++ modules/keda/variables.tf
@@ -0,0 +1,4 @@
+variable "chart_version" {
+  type    = string
+  default = "2.16.0"
+}
